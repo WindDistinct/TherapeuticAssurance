5101f49ac9d5413cbbdfc3bd0f4dc53d53f0fd89
Nicolas A. <winddistinct@outlook.com>
2024-05-20 23:25:02 -0500

    Initialization

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Header from "../components/ui/Header";
+export default function Home() {
+  return (
+
+          <>
+
+            <Header/>
+           
+
+          </>
+            
+
+
+        );
+}
---
+++ app/ruta/page.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+
+
+
+export const metadata: Metadata = {
+    title: "RUTA 02",
+    description: "Farmacia dedicada a la venta de todo tipo de medicamentos",
+  };
+  
+
+
+
+export default function Ruta2() {
+
+  
+
+    return (
+  
+                <>
+
+                  
+
+                    <h1>soy ruta 02 ...</h1>
+                
+                </>
+              
+  
+  
+          );
+  }
+  
